46154f9953a254804f2008278229046d54770099
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-07-28 14:34:04 +0600

    this file not a code, this file is only comments for all variables

---
+++ Larning Arduino code/5th_tutorial/5th_tutorial.ino
@@ -0,0 +1,78 @@
+/* larnign tutorial link 
+https://www.youtube.com/watch?v=0rPaiyzyQHU&list=PLV3C-t_tgjGFyXP_-AF37AoIuxM9jzELM
+1. byte 
+  a byte stores an 8-bit unsigned number, form 0 ti 255(maximum value of (2^8)-1)
+  11111111=>255
+  Syntax: byte val= value;
+  val: variable name
+  value: the value to assgn to that variable
+  ex: byte var =155;
+2. word
+  a vard can store an unsigned number of at least 16 bit from 0 ti 65535(maximum value of (2^16)-1)
+  Syntax : word var= value;
+  var: variable name
+  value: the value to assign to that variable
+  ex: woed var= 2568;
+3. short 
+  a short is a 16-bit data-type.
+  a short stores a 16-bit(2-byte) value. This yields a range of -32768 to 32767
+  (minimum value of -2^15 and a maximum value of(2^15)-1).
+  Syntx: short var = value;
+  var: varibale name 
+  value: the value to assign to that variable 
+  Ex: short var= -1552;
+4. int 
+  a short stores a 16-bit(2-byte) value. This yields a range of -32768 to 32767
+  (minimum value of -2^15 and a maximum value of(2^15)-1).
+  Syntax: int var=value;
+  var: variable name
+  value: the value to assign to that variable
+  ex: int var=-282;
+5. long 
+  long variables are extended size variables for number storage, and store 32 
+  bit(4 bytes), from -2,147,483,648 to 2,147,483,647(minimum value of -2^31 
+  and a maximum value of (2^31)-1).
+  Suntax: long var = value;
+  var: variable name
+  value: the value to assign to that variable
+  ex: long var=-25682;
+6. float  
+  Datatype for floating-point numbers, a number that has a decimal point 
+  floating-point numbers can be as large as 3.4028235*10^38 and as low as
+  -3.4028235*10^38
+  Syntx: float var= value;
+  var: variable name
+  value: the value to assign to that variable
+  ex: float var= 25.26;
+7. double 
+  the double inmplementastin is exactly to same as to float.
+  Syntax: double var= value;
+  var: variable name 
+  value: the value to assign to that variable 
+  ex: double var = 25.26; 
+8. unsigned int 
+  unsigned ints(unsigned integers) are the same as ints in that they store a 2
+  byte value. Instead of storing negative numbers however they only sotre 
+  positive values, yielding a usefun range of 0 to 65,535((2^16)-1)
+  Syntax: unsigned int var= value;
+  var: variable name 
+  value: the value to assign to the variavble
+  ex: unsigned int var= 15;
+9. unsigned long 
+  unsigned long variables are extended size variables for number storage, and 
+  store 32 bits(4 bytes), Unlike standard longs unsigned longs won't sotre
+  negative numbers, makeing their range form 0 ot 4,294,967,296(2^32-1)
+  Syntax: unsigned long var = value;
+  var : variable name
+  value: the value to assign to that variable 
+  Ex: unsigned long var= 2525487; 
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
