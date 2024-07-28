070276d7dd6e4396bde5494b455df3066f0cc00d
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-07-28 17:03:05 +0600

    again commit 2nd tutorial larnig about varialbes

---
+++ Larning Arduino code/Tutorial_Codes/2nd_tutorial.ino
@@ -0,0 +1,18 @@
+/*tutolial link : https://www.youtube.com/watch?v=TfsoOpSTh7E&list=PLV3C-t_tgjGFyXP_-AF37AoIuxM9jzELM
+larnign key 
+1. Using variables/ Variables are used to store information to be referenced & manipulated in a computer program.
+*/
+// data_tupe(integer = int ) variable_name = value
+// int val; in the variabel inisilaiz value 0
+int val = 10;
+// global variables 
+void setup() {
+  // put your setup code here, to run once:
+  int val2 = 20; 
+  //local variables 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
